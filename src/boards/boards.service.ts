e0643d7ba571bf1a14bdf53c29c856830f77e936
Hyun-hyo-min <hyomin2677@gmail.com>
2024-07-29 22:15:34 +0900

    Feat: board module

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BoardStatus } from './board-status-enum';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+
+@Injectable()
+export class BoardsService {
+    //Inject Repository to service
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository,
+    ) { }
+
+    getAllBoards(): Promise<Board[]> {
+        return this.boardRepository.find();
+    }
+
+    async getBoardById(id: number): Promise<Board> {
+        const board = await this.boardRepository.findOne({ where: { id } });
+
+        if (!board) {
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+
+        return board;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardRepository.createBoard(createBoardDto);
+    }
+
+    async deleteBoard(id: number): Promise<void> {
+        const result = await this.boardRepository.delete({ id });
+
+        if (result.affected === 0) {
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+    }
+
+
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        const board = await this.getBoardById(id);
+        board.status = status;
+        await this.boardRepository.save(board);
+
+        return board;
+    }
+}
